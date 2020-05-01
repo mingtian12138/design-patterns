ea2dacd7ef4ab65ddc31311757ad766583c26ebf
mingtian12138 <643091168@qq.com>
2020-05-01 23:12:16 +0800

    Add files via upload

---
+++ Bridge/White.java
@@ -0,0 +1,9 @@
+package Bridge;
+
+public class White implements Color
+{
+    @Override
+    public void bePaint(String penType, String name) {
+        System.out.println(penType+" White "+name);
+    }
+}
