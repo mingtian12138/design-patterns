202f628587331082d26d5fabaddb382b63c409ce
mingtian12138 <643091168@qq.com>
2020-05-01 23:15:29 +0800

    Add files via upload

---
+++ Visitor/XMLUtil.java
@@ -0,0 +1,37 @@
+package Visitor;
+
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.File;
+import java.io.IOException;
+
+public class XMLUtil {
+    public static Object getBean() throws ParserConfigurationException {
+        try {
+            DocumentBuilderFactory dFactory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder builder =dFactory.newDocumentBuilder();
+            Document doc;
+            doc= builder.parse(new File("design patterns public/src/Visitor/config.xml"));
+
+            NodeList nl =doc.getElementsByTagName("className");
+
+            Node classNode=nl.item(0).getFirstChild();
+            String cName =classNode.getNodeValue();
+
+            Class c=Class.forName(cName);
+            Object obj=c.newInstance();
+            return  obj;
+
+        } catch (SAXException | ClassNotFoundException | IOException | IllegalAccessException | InstantiationException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
