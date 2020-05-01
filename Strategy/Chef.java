8719a218827ce22995b217b03c111f3b317ceba7
mingtian12138 <643091168@qq.com>
2020-05-01 21:42:59 +0800

    Add files via upload

---
+++ Strategy/Chef.java
@@ -0,0 +1,11 @@
+package Strategy;
+
+public class Chef {
+    private ICutFruit cutMethod;
+    public void SetCutMethod(ICutFruit cutMethod){
+        this.cutMethod=cutMethod;
+    }
+    public void CutFruit(String fruitName){
+        cutMethod.CutStrategy(fruitName);
+    }
+}
