241485a95ae8b95d5db26d2bab959ed579a1a802
mingtian12138 <643091168@qq.com>
2020-05-05 12:37:44 +0800

    Add files via upload

---
+++ Factory/ClientClass.java
@@ -0,0 +1,30 @@
+
+package Factory;
+
+
+import javax.xml.XMLConstants;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.stream.XMLReporter;
+import java.beans.XMLDecoder;
+
+public class ClientClass {
+    public static void main(String[] args) throws ParserConfigurationException {
+        Factory factory =new Factory();
+        Factory bfactory =new BFactory();
+
+        Fruit fruit=(Fruit)XMLUtil.getBean();
+        fruit.eat();
+        Fruit fruit2=factory.CreateFruit("B");
+        fruit2.eat();
+    }
+    public String factory(String fruitName){
+        if (fruitName.equals("Apple")){
+            return "Apple";
+        }
+        if (fruitName.equals("Banana")){
+            return "Banana";
+        }
+
+        return null;
+    }
+}
---
+++ Factory/Fruit.java
@@ -0,0 +1,8 @@
+
+package Factory;
+
+public class Fruit {
+    void eat(){
+        System.out.println("eat Fruit!");
+    }
+}
